ef590f2e75ee219ec9d19295bc023d93d0207af7
idris <mokh015@hotmail.com>
2018-01-29 18:33:56 +0100

    Initial commit

---
+++ src/EchoClientTCP.java
@@ -0,0 +1,90 @@
+/**
+ * Socket programming example: TCP Client
+ * DATS/ITPE2410 Networking and Cloud Computing, Spring 2018
+ * Raju Shrestha, HiOA
+ **/
+import java.io.*;
+import java.net.*;
+
+public class EchoClientTCP
+{
+    public static void main(String[] args) throws IOException
+    {
+
+        String hostName = "127.0.0.1"; // Default host, localhost
+        int portNumber = 5555; // Default port to use
+        if (args.length > 0)
+        {
+            hostName = args[0];
+            if (args.length > 1)
+            {
+                portNumber = Integer.parseInt(args[1]);
+                if (args.length > 2)
+                {
+                    System.err.println("Usage: java EchoClientTCP [<host name>] [<port number>]");
+                    System.exit(1);
+                }
+            }
+        }
+
+
+        System.out.println("Hi, I am EchoUCase TCP client!");
+
+        try
+                (
+                        // create TCP socket for the given hostName, remote port PortNumber
+                        Socket clientSocket = new Socket(hostName, portNumber);
+
+                        // Stream writer to the socket
+                        PrintWriter out =
+                                new PrintWriter(clientSocket.getOutputStream(), true);
+                        // Stream reader from the socket
+                        BufferedReader in =
+                                new BufferedReader(
+                                        new InputStreamReader(clientSocket.getInputStream()));
+                        // Keyboard input reader
+                        BufferedReader stdIn =
+                                new BufferedReader(
+                                        new InputStreamReader(System.in))
+                )
+        {
+            String userInput;
+            // Loop until null input string
+            System.out.print("I (Client) [" + InetAddress.getLocalHost()  + ":" + clientSocket.getLocalPort() + "] > ");
+            while ((userInput = stdIn.readLine()) != null && !userInput.isEmpty())
+            {
+                // write keyboard input to the socket
+                out.println(userInput);
+
+                // read from the socket and display
+                String receivedText = in.readLine();
+
+                System.out.println("Server [" + hostName +  ":" + portNumber + "] > " + receivedText);
+                System.out.print("I (Client) [" + clientSocket.getLocalAddress().getHostAddress() + ":" + clientSocket.getLocalPort() + "] > ");
+            }
+        } catch (UnknownHostException e)
+        {
+            System.err.println("Unknown host " + hostName);
+            System.exit(1);
+        } catch (IOException e)
+        {
+            System.err.println("Couldn't get I/O for the connection to " + hostName);
+            System.exit(1);
+        }
+    }
+
+    public static convert( double value1, double value2, String currency){
+
+        if(currency.equals("NOK")){
+            value2 = value1 * 0.734878047;
+            System.out.println(value1 + "NOK = " + value2 + " USA. (Conversion rate: 1 NOK = 0.734878047 USD)");
+
+            System.out.println("Conversion rate: 1 NOK = 0.734878047 USD"
+                    + "\nConversion rate: USD = 1.36077 Nok");
+        }
+
+
+
+
+    }
+}
